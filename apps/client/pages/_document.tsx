3886754e7fe510a7b53152651fef8692353c4c81
amosbastian <amosbastian@gmail.com>
2020-05-24 00:49:24 +0200

    Initial commit

---
+++ apps/client/pages/_document.tsx
@@ -0,0 +1,34 @@
+import React, { ReactElement } from "react";
+import Document, { Head, Main, NextScript } from "next/document";
+import { ServerStyleSheet } from "styled-components";
+
+export default class CustomDocument extends Document<{
+  styleTags: ReactElement[];
+}> {
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+
+    const page = renderPage((App) => (props) =>
+      sheet.collectStyles(<App {...props} />),
+    );
+
+    const styleTags = sheet.getStyleElement();
+
+    return { ...page, styleTags };
+  }
+
+  render() {
+    return (
+      <html>
+        <Head>
+          <title>Welcome to client!</title>
+          {this.props.styleTags}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}

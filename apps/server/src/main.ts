711dbe51e22fbc3a1285eaa6a3c6e56c06f5fed4
amosbastian <amosbastian@gmail.com>
2020-05-24 21:07:53 +0200

    Add ApolloServer to main

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,18 +1,49 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
+import "reflect-metadata";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema, Resolver, Query } from "type-graphql";
+import { createConnection } from "typeorm";
+import { User } from "./entity/User";
+import { RegisterResolver } from "./modules/user/Register";
 
-import * as express from "express";
+@Resolver()
+class HelloWorldResolver {
+  @Query(() => String)
+  async hello() {
+    return "Hello, World";
+  }
+}
 
-const app = express();
+const main = async () => {
+  await createConnection({
+    name: "default",
+    type: "postgres",
+    host: "localhost",
+    port: 5432,
+    username: "postgres",
+    password: "postgres",
+    database: "nextjs-graphql-starter",
+    logging: true,
+    synchronize: true,
+    entities: [User],
+  });
 
-app.get("/api", (req, res) => {
-  res.send({ message: "Welcome to server!" });
-});
+  const schema = await buildSchema({
+    resolvers: [HelloWorldResolver, RegisterResolver],
+  });
 
-const port = process.env.port || 3333;
-const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
-});
-server.on("error", console.error);
+  const apolloServer = new ApolloServer({ schema });
+
+  const app = express();
+
+  apolloServer.applyMiddleware({ app });
+
+  const port = process.env.port || 3333;
+
+  const server = app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}/graphql`);
+  });
+  server.on("error", console.error);
+};
+
+main();

17edb0cb76ab1bcc4f13b7df61480472a382acb9
amosbastian <amosbastian@gmail.com>
2020-05-31 18:38:32 +0200

    Update styling of login page

--- apps/client/pages/Login.tsx
+++ apps/client/pages/Login.tsx
@@ -1,19 +1,58 @@
 import React from "react";
+import styled from "styled-components";
+import AuthenticationLayout from "../components/authentication-layout/authentication-layout";
+import LoginForm from "../components/login-form/login-form";
 import withApollo from "../apollo/withApollo";
-import AuthenticationLayout from "../components/AuthenticationLayout";
+import Link from "next/link";
+
+const StyledSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledNav = styled.nav`
+  padding: 1rem;
+  position: absolute;
+  top: 0;
+  right: 0;
+`;
+
+const StyledMain = styled.main`
+  display: grid;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 1rem;
+`;
 
 export const Login = () => {
   return (
     <AuthenticationLayout>
-      <section>
-        <nav></nav>
-        <header>
-          <h1>Welcome to client!</h1>
-        </header>
-        <main>Login</main>
-      </section>
+      <StyledSection>
+        <StyledNav>
+          Not a member?{" "}
+          <Link href="signup">
+            <a>Sign up now!</a>
+          </Link>
+        </StyledNav>
+        <StyledDiv>
+          <header>
+            <h1>Login to client!</h1>
+          </header>
+          <StyledMain>
+            <LoginForm />
+          </StyledMain>
+        </StyledDiv>
+      </StyledSection>
     </AuthenticationLayout>
   );
 };
 
-export default Login;
+export default withApollo(Login);

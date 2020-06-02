e8b998776139914bdf20a8711bf5964fa897d077
amosbastian <amosbastian@gmail.com>
2020-06-02 23:26:08 +0200

    Update site layout

--- apps/client/components/site-layout/site-layout.tsx
+++ apps/client/components/site-layout/site-layout.tsx
@@ -1,13 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-import { NextPage } from "next";
+import useTheme from "@material-ui/core/styles/useTheme";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+
+interface LayoutContainerProps {
+  isDesktop: boolean;
+}
+
+const LayoutContainer = styled.div<LayoutContainerProps>`
+  padding-top: 56px;
+  height: 100%;
+  min-height: 100vh;
+
+  ${({ theme }) => theme.breakpoints.up("sm")} {
+    padding-top: 64px;
+  }
+  padding-left: ${({ isDesktop }) => (isDesktop ? 240 : 0)}px;
+`;
+
+const MainContainer = styled.main`
+  height: 100%;
+`;
 
 const SiteLayout: React.FC = ({ children }) => {
+  const [openSidebar, setOpenSidebar] = useState<boolean>(false);
+
+  const theme = useTheme();
+
+  const isDesktop = useMediaQuery(theme.breakpoints.up("md"), {
+    defaultMatches: true,
+  });
+
+  const handleSidebarOpen = () => {
+    setOpenSidebar(true);
+  };
+
+  const handleSidebarClose = () => {
+    setOpenSidebar(false);
+  };
+
+  const sidebarIsOpen = isDesktop ? true : openSidebar;
+
   return (
-    <div>
-      Site layout
-      <div>{children}</div>
-    </div>
+    <LayoutContainer isDesktop={isDesktop}>
+      <MainContainer>{children}</MainContainer>
+    </LayoutContainer>
   );
 };
 

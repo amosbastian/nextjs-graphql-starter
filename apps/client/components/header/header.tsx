233b8c9f05be4cbb8f6c6aca7333c28a0e1f1b50
amosbastian <amosbastian@gmail.com>
2020-06-03 22:54:38 +0200

    Remove sidebar from site layout

--- apps/client/components/header/header.tsx
+++ apps/client/components/header/header.tsx
@@ -3,33 +3,20 @@ import styled from "styled-components";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
-import Hidden from "@material-ui/core/Hidden";
 
 const StyledAppBar = styled(AppBar)`
   box-shadow: none !important;
 `;
 
-interface HeaderProps {
-  onSidebarOpen: () => void;
-}
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface HeaderProps {}
 
-const Header: React.FC<HeaderProps> = ({ onSidebarOpen }) => {
+const Header: React.FC<HeaderProps> = () => {
   return (
     <StyledAppBar>
       <Toolbar>
         <Typography variant="h6">nextjs-graphql-starter</Typography>
         <div style={{ flexGrow: 1 }} />
-        <Hidden mdUp>
-          <IconButton
-            aria-label="open sidebar"
-            color="inherit"
-            onClick={onSidebarOpen}
-          >
-            <MenuIcon />
-          </IconButton>
-        </Hidden>
       </Toolbar>
     </StyledAppBar>
   );
--- apps/client/components/site-layout/site-layout.tsx
+++ apps/client/components/site-layout/site-layout.tsx
@@ -1,8 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import useTheme from "@material-ui/core/styles/useTheme";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
-import Sidebar from "../sidebar/sidebar";
 import Header from "../header/header";
 import Footer from "../footer/footer";
 
@@ -15,12 +14,10 @@ const LayoutContainer = styled.div<LayoutContainerProps>`
   height: 100%;
   min-height: 100vh;
   display: grid;
-  grid-template-rows: max-content 1fr;
 
   ${({ theme }) => theme.breakpoints.up("sm")} {
     padding-top: 64px;
   }
-  padding-left: ${({ isDesktop }) => (isDesktop ? 240 : 0)}px;
 `;
 
 const MainContainer = styled.main`
@@ -29,32 +26,15 @@ const MainContainer = styled.main`
 `;
 
 const SiteLayout: React.FC = ({ children }) => {
-  const [openSidebar, setOpenSidebar] = useState<boolean>(false);
-
   const theme = useTheme();
 
   const isDesktop = useMediaQuery(theme.breakpoints.up("md"), {
     defaultMatches: true,
   });
 
-  const handleSidebarOpen = () => {
-    setOpenSidebar(true);
-  };
-
-  const handleSidebarClose = () => {
-    setOpenSidebar(false);
-  };
-
-  const sidebarIsOpen = isDesktop ? true : openSidebar;
-
   return (
     <LayoutContainer isDesktop={isDesktop}>
-      <Header onSidebarOpen={handleSidebarOpen} />
-      <Sidebar
-        onClose={handleSidebarClose}
-        open={sidebarIsOpen}
-        variant={isDesktop ? "persistent" : "temporary"}
-      />
+      <Header />
       <MainContainer>
         {children}
         <Footer />

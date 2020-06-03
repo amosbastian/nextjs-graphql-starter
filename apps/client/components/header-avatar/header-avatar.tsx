4ecbc4c4a5ede19f506d3aba3a923d8fb90b95d3
amosbastian <amosbastian@gmail.com>
2020-06-03 23:41:35 +0200

    Add menu to HeaderAvatar

--- apps/client/components/header-avatar/header-avatar.tsx
+++ apps/client/components/header-avatar/header-avatar.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import styled from "styled-components";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
 import { gql } from "apollo-boost";
 import { useQuery } from "@apollo/react-hooks";
 import { UsernameQuery } from "@nextjs-graphql-starter/codegen";
@@ -34,13 +36,51 @@ const USERNAME = gql`
 `;
 
 export const HeaderAvatar = () => {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(
+    null,
+  );
+
   const { data } = useQuery<UsernameQuery>(USERNAME);
 
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
-    <StyledButton>
-      <StyledAvatar alt="Amos Bastian" />
-      {data?.me.username}
-    </StyledButton>
+    <div>
+      <StyledButton
+        aria-controls="user-menu"
+        aria-haspopup="true"
+        onClick={handleClick}
+      >
+        <StyledAvatar alt="Amos Bastian" />
+        {data?.me.username}
+      </StyledButton>
+      <Menu
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+        id="user-menu"
+        getContentAnchorEl={null}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "center",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "center",
+        }}
+      >
+        <MenuItem>Settings</MenuItem>
+        <MenuItem>Account</MenuItem>
+        <MenuItem>Logout</MenuItem>
+      </Menu>
+    </div>
   );
 };
 

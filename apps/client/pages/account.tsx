d5cf9e7b1e2f02b69bdf9bdc18d07e206d45b4cb
amosbastian <amosbastian@gmail.com>
2020-06-04 23:30:42 +0200

    Update account page to include tabs with tab panels

--- apps/client/pages/account.tsx
+++ apps/client/pages/account.tsx
@@ -1,15 +1,33 @@
 import React from "react";
 import SiteLayout from "../components/site-layout/site-layout";
-import styled from "styled-components";
 import withApollo from "../apollo/withApollo";
+import AccountTabs from "../components/account-tabs/account-tabs";
+import AccountTabPanel from "../components/account-tab-panel/account-tab-panel";
+import AccountGeneral from "../components/account-general/account-general";
+import AccountSecurity from "../components/account-security/account-security";
 
 /* eslint-disable-next-line */
 export interface AccountProps {}
 
-export const Account = (props: AccountProps) => {
+export const Account: React.FC<AccountProps> = () => {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (
+    event: React.ChangeEvent<{}>,
+    newValue: number,
+  ) => {
+    setValue(newValue);
+  };
+
   return (
     <SiteLayout>
-      <h1>Welcome to Account!</h1>
+      <AccountTabs onChange={handleChange} value={value} />
+      <AccountTabPanel value={value} index={0}>
+        <AccountGeneral />
+      </AccountTabPanel>
+      <AccountTabPanel value={value} index={1}>
+        <AccountSecurity />
+      </AccountTabPanel>
     </SiteLayout>
   );
 };

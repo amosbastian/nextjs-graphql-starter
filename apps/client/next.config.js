ab3eb875fe0778c1a1f9aee8a7f2b94bd04ee4c1
amosbastian <amosbastian@gmail.com>
2020-06-08 00:13:25 +0200

    Update next.config.js so environment variables work

--- apps/client/next.config.js
+++ apps/client/next.config.js
@@ -1,1 +1,8 @@
-module.exports = {};
+module.exports = {
+  env: {
+    cloudinaryCloudName: process.env.CLOUDINARY_CLOUD_NAME,
+    cloudinaryUploadPreset: process.env.CLOUDINARY_UPLOAD_PRESET,
+    cloudinaryApiKey: process.env.CLOUDINARY_API_KEY,
+    cloudinaryApiSecret: process.env.CLOUDINARY_API_SECRET,
+  },
+};

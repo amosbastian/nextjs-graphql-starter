9c85a39dfabf831dc9e40c285325fae20f6a8393
amosbastian <amosbastian@gmail.com>
2020-06-09 00:54:05 +0200

    Move theme to @client/ui

---
+++ apps/client/ui/theme.ts
@@ -0,0 +1,10 @@
+import {
+  createMuiTheme,
+  ThemeOptions,
+} from "@material-ui/core/styles";
+
+const theme = createMuiTheme({});
+
+export const themeOptions: ThemeOptions = {};
+
+export default theme;
